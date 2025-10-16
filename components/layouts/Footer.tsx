b00a1c8723e48ef77251f17f741f81a22943bc4b
Giovidoh <giovidoh@gmail.com>
2025-10-16 20:49:44 +0000

    Enhance Footer component layout by adding SectionContainer and BaseButton elements for improved structure and functionality, including options to download CV and contact.

--- components/layouts/Footer.tsx
+++ components/layouts/Footer.tsx
@@ -1,5 +1,20 @@
+import BaseButton from '@/components/buttons/BaseButton';
+import SectionContainer from '@/components/containers/SectionContainer';
+
 const Footer = () => {
-    return <div>Footer</div>;
+  return (
+    <footer className="border-t">
+      <SectionContainer className="pt-20 pb-40">
+        <div className="flex w-full justify-between gap-5 max-sm:flex-col max-sm:items-center">
+          <div>Cir-Giovanni IDOH</div>
+          <div className="flex flex-col items-end gap-4 max-sm:items-center">
+            <BaseButton>Téléchargez mon CV</BaseButton>
+            <BaseButton>Contactez-moi</BaseButton>
+          </div>
+        </div>
+      </SectionContainer>
+    </footer>
+  );
 };
 
 export default Footer;
